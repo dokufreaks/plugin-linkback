8b8b8c47a03d7ad527634d0ba4815b910f9259c9
DokuWiki Translation <>
2013-08-01 19:01:18 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['linkback_enabledisable'] = 'Linkbacks für externe Links senden';
+$lang['linkbacks']             = 'Linkbacks';
+$lang['linkback_type_pingback'] = 'Pingback';
+$lang['linkback_type_trackback'] = 'Trackback';
+$lang['trackback_url']         = 'Sende manuelle Trackbacks an folgende URL:';
+$lang['btn_hide']              = 'Verstecken';
+$lang['btn_show']              = 'Zeigen';
+$lang['btn_delete']            = 'Löschen';
+$lang['btn_ham']               = 'Als Nicht-Spam melden';
+$lang['btn_spam']              = 'Als Spam melden';
+$lang['btn_change']            = 'Ändern';
+$lang['mail_newlinkback']      = 'Neue Linkback';
+$lang['linkback_singular']     = 'Linkback';
+$lang['linkback_plural']       = 'Linkbacks';
+$lang['send']                  = 'Senden';
+$lang['receive']               = 'Empfangen';
+$lang['display']               = 'Anzeigen';
+$lang['status']                = 'Status';
+$lang['older']                 = 'Ältere Linkbacks';
+$lang['newer']                 = 'Neuere Linkbacks';
+$lang['menu']                  = 'Linkbacks verwalten';
