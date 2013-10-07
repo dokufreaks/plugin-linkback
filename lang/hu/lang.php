eafc1738ec2ace08f685987d468e4d370f20a3d2
Serenity87HUN <anikototh87@gmail.com>
2013-10-07 15:40:05 +0200

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Serenity87HUN <anikototh87@gmail.com>
+ */
+$lang['btn_hide']              = 'Elrejt';
+$lang['btn_show']              = 'Mutat';
+$lang['btn_delete']            = 'Töröl';
+$lang['btn_spam']              = 'Spam jelentése';
+$lang['btn_change']            = 'Változtatás';
+$lang['send']                  = 'Küldés';
+$lang['receive']               = 'Fogadás';
+$lang['display']               = 'Megjelenítés';
+$lang['status']                = 'Állapot';
