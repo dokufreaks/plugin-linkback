e84cec4e530d166d978af2a162fe4b251475a9b8
Egmont Schreiter <egmont.schreiter@gmx.de>
2013-10-29 07:55:06 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,7 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
+ * @author Egmont Schreiter <egmont.schreiter@gmx.de>
  */
 $lang['enable_pingback']       = 'Pingbacks aktivieren.';
 $lang['enable_trackback']      = 'Trackbacks aktivieren.';
 $lang['order']                 = 'Reihenfolge in der Linkbacks gesendet werden sollen.';
+$lang['akismet_apikey']        = 'Der <a href="http://wordpress.com/signup/">Akismet API Schlüssel</a>. Das Plugin funktioniert ohne ihn nicht!';
+$lang['akismet_moderate']      = 'Wenn Akismet einen Linkback als Spam einschätzt, moderiere ihn anstatt zu löschen.';
