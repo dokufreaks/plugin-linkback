2fb5add870194e399d86c8217aeb535fec688341
Nicolas Friedli <nicolas@theologique.ch>
2014-05-14 11:35:05 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['linkback_enabledisable'] = 'Envoyer des linkbacks pour les liens externes';
+$lang['linkbacks']             = 'Linkbacks';
+$lang['linkback_type_pingback'] = 'Pingbacks';
+$lang['linkback_type_trackback'] = 'Trackback';
+$lang['trackback_url']         = 'Utiliser l\'URL pour envoyer des trackbacks manuellement:';
+$lang['btn_hide']              = 'Cacher';
+$lang['btn_show']              = 'Montrer';
+$lang['btn_delete']            = 'Effacer';
+$lang['btn_ham']               = 'Signaler comme acceptable';
+$lang['btn_spam']              = 'Signaler comme indésirable';
+$lang['btn_change']            = 'Changer';
+$lang['mail_newlinkback']      = 'Nouveau linkback';
+$lang['linkback_singular']     = 'Linkback';
+$lang['linkback_plural']       = 'Linkbacks';
+$lang['send']                  = 'Envoyer';
+$lang['receive']               = 'Recevoir';
+$lang['display']               = 'Afficher';
+$lang['status']                = 'Status';
+$lang['older']                 = 'Anciens linkbacks';
+$lang['newer']                 = 'Nouveaux linkbacks';
+$lang['menu']                  = 'Modérer les linkbacks';
